87164d6967e25b897c5c2fed93cfdf71b89ef034
ilaumjd <ilaumjd@gmail.com>
2024-11-09 11:53:58 +0700

    validate chirp

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -20,6 +21,8 @@ func main() {
 	mux.HandleFunc("POST /admin/reset", apiCfg.handleReset)
 
 	mux.HandleFunc("GET /api/healthz", handleHealthz)
+
+	mux.HandleFunc("POST /api/validate_chirp", handleValidateChirp)
 
 	srv := &http.Server{
 		Addr:    ":8080",
@@ -58,4 +61,40 @@ func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
 
 func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
+}
+
+func handleValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	_ = decoder.Decode(&params)
+
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+	respBody := returnVals{
+		Valid: true,
+	}
+	respondWithJSON(w, 200, respBody)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"error": "` + msg + `"}`))
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	response, _ := json.Marshal(payload)
+	w.Write(response)
 }
